733b8d8c807d5ce5900c2d668401011ca5ab3b09
Sridhar Sarnobat <ss401533@gmail.com>
2019-06-20 11:44:02 -0700

    Minor changes (untested)

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-12.0.1.jdk/Contents/Home /JasperSystem/Tools/gradle-5.4.1/bin/gradle build
+JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-12.0.1.jdk/Contents/Home 
+
+/JasperSystem/Tools/gradle-5.4.1/bin/gradle build
+java -jar /Users/srsarnob/github/csv2txt_tree/build/libs/csv2txt_tree.jar
